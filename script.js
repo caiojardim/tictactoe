788f85f62a0659f65db2615fe7d59969b1209088
Caio Jardim <caiojardinho@gmail.com>
2021-06-21 15:14:03 -0300

    get each element of the game board by id

---
+++ script.js
@@ -0,0 +1,11 @@
+const r1c1 = document.querySelector("#r1c1");
+const r1c2 = document.querySelector("#r1c2");
+const r1c3 = document.querySelector("#r1c3");
+
+const r2c1 = document.querySelector("#r2c1");
+const r2c2 = document.querySelector("#r2c2");
+const r2c3 = document.querySelector("#r2c3");
+
+const r3c1 = document.querySelector("#r3c1");
+const r3c2 = document.querySelector("#r3c2");
+const r3c3 = document.querySelector("#r3c3");
