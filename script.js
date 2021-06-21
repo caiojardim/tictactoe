04d2cc251c0be122daaa1888681e72de55185a29
Caio Jardim <caiojardinho@gmail.com>
2021-06-21 17:20:59 -0300

    create eventListener to manitulate the click of each button

--- script.js
+++ script.js
@@ -9,3 +9,10 @@ const r2c3 = document.querySelector("#r2c3");
 const r3c1 = document.querySelector("#r3c1");
 const r3c2 = document.querySelector("#r3c2");
 const r3c3 = document.querySelector("#r3c3");
+
+elements = document.querySelectorAll("button");
+elements.forEach((e) =>
+	e.addEventListener("click", function (e) {
+		console.log(e.currentTarget.attributes);
+	})
+);
