3449ec90666067b7ffee8de36900d9db57982d38
Caio Jardim <caiojardinho@gmail.com>
2021-06-28 13:31:09 -0300

    Now can be inserted X or O with the click, toggle betten X and O and validade to not change the value if already exist

--- script.js
+++ script.js
@@ -10,9 +10,24 @@ const r3c1 = document.querySelector("#r3c1");
 const r3c2 = document.querySelector("#r3c2");
 const r3c3 = document.querySelector("#r3c3");
 
+const times = '<i class="fas fa-times"></i>';
+const circle = '<i class="far fa-circle"></i>';
+
+let timesOrCircle = times;
+
 elements = document.querySelectorAll("button");
 elements.forEach((e) =>
 	e.addEventListener("click", function (e) {
-		console.log(e.currentTarget.attributes);
+		const element = e.currentTarget;
+		const hasX = element.attributes.x.value === "true";
+		const hasO = element.attributes.o.value === "true";
+
+		timesOrCircle = timesOrCircle === times ? circle : times;
+
+		if (!(hasX || hasO)) {
+			element.innerHTML = timesOrCircle;
+			element.setAttribute("x", "true");
+		}
+		console.log(!(hasX || hasO));
 	})
 );
