88871bd29722cbaf6c11639c265a09853533ce16
Caio Jardim <caiojardinho@gmail.com>
2021-06-29 16:16:52 -0300

    created some functions to check if the game has a winner

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
-const r1c1 = document.querySelector("#r1c1");
-const r1c2 = document.querySelector("#r1c2");
-const r1c3 = document.querySelector("#r1c3");
+function isAttributesTrue(value1, value2, value3, player) {
+	v1 = value1.attributes[player].value === "true";
+	v2 = value2.attributes[player].value === "true";
+	v3 = value3.attributes[player].value === "true";
+	return v1 && v2 && v3;
+}
 
-const r2c1 = document.querySelector("#r2c1");
-const r2c2 = document.querySelector("#r2c2");
-const r2c3 = document.querySelector("#r2c3");
+function checkWinner() {
+	const r1c1 = document.querySelector("#r1c1");
+	const r1c2 = document.querySelector("#r1c2");
+	const r1c3 = document.querySelector("#r1c3");
 
-const r3c1 = document.querySelector("#r3c1");
-const r3c2 = document.querySelector("#r3c2");
-const r3c3 = document.querySelector("#r3c3");
+	const r2c1 = document.querySelector("#r2c1");
+	const r2c2 = document.querySelector("#r2c2");
+	const r2c3 = document.querySelector("#r2c3");
+
+	const r3c1 = document.querySelector("#r3c1");
+	const r3c2 = document.querySelector("#r3c2");
+	const r3c3 = document.querySelector("#r3c3");
+
+	if (isAttributesTrue(r1c1, r1c2, r1c3, "x")) return "x";
+	if (isAttributesTrue(r2c1, r2c2, r2c3, "x")) return "x";
+	if (isAttributesTrue(r3c1, r3c2, r3c3, "x")) return "x";
+
+	if (isAttributesTrue(r1c1, r2c1, r3c1, "x")) return "x";
+	if (isAttributesTrue(r1c2, r2c2, r3c2, "x")) return "x";
+	if (isAttributesTrue(r1c3, r2c3, r3c3, "x")) return "x";
+
+	if (isAttributesTrue(r1c1, r2c2, r3c3, "x")) return "x";
+	if (isAttributesTrue(r1c3, r2c2, r3c1, "x")) return "x";
+
+	// retorna x, o, tie, true
+}
 
 const times = '<i class="fas fa-times"></i>';
 const circle = '<i class="far fa-circle"></i>';
@@ -28,13 +50,15 @@ elements.forEach((e) =>
 		if (!(hasX || hasO)) {
 			if (turn === "o") {
 				element.innerHTML = circle;
-				element.setAttribute("x", "true");
+				element.setAttribute("o", "true");
 				turn = "x";
 			} else if (turn === "x") {
 				element.innerHTML = times;
-				element.setAttribute("o", "true");
+				element.setAttribute("x", "true");
 				turn = "o";
 			}
 		}
+
+		console.log(checkWinner());
 	})
 );
